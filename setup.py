acb3052d2f8848af0f5f15b99fd563b5060ef6fa
scantistdemo007 <55232587+scantistdemo007@users.noreply.github.com>
2020-03-18 10:26:22 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,6 @@ from setuptools import setup
 setup(
     name = 'python-with-setup-file-example',
     install_requires = [
-        'django==1.7.1',
-        'PyJWT==0.4.2',
-        'rsa==3.4',
-        'requests==2.2.1',
-        'feedparser==5.1.1',
-        'Flask==0.11.1'
+        'urllib3>=1.21.1,<1.26,!=1.25.0,!=1.25.1',
     ]
 )
